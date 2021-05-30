763bd9102c48eae0b485d045dc98941108cbf011
KindaExists <kindaexists@gmail.com>
2021-05-30 17:23:46 +0800

    added (simple) solution for 1-easy

---
+++ 1-easy/1-easy.py
@@ -0,0 +1,13 @@
+
+"""
+[easy] challenge #1
+
+Source / Reddit Post - https://www.reddit.com/r/dailyprogrammer/comments/pih8x/easy_challenge_1/
+"""
+
+#! Assuming users input correct values 
+name = input('What is your name?: ')
+age = int(input('What is your age?: '))
+user = input('What is your username?: ')
+
+print(f'your name is {name}, you are {age} years old, and your username is {user}')
