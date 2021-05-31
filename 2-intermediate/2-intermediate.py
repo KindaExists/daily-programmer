7cec7ddf6fe33df40c8ce4bd4d0b6885b358695e
KindaExists <kindaexists@gmail.com>
2021-05-31 10:39:17 +0800

    created 2-intermediate file

---
+++ 2-intermediate/2-intermediate.py
@@ -0,0 +1,10 @@
+
+"""
+[intermediate] challenge #2
+
+Source / Reddit Post - https://www.reddit.com/r/dailyprogrammer/comments/pjbuj/intermediate_challenge_2/
+"""
+
+#! Yeah... No thanks, I'm not doing this in C
+# Most likely will be passing on this challenge, since it is too time-consuming to create a full story
+# I have an idea to make this program recursive
