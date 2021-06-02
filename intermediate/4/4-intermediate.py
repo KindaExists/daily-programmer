c6cb78a1616f9084bd2da72b5aa8d3f3ad3fda52
KindaExists <kindaexists@gmail.com>
2021-06-02 13:20:42 +0800

    created (cheap) solution to 4-intermediate

---
+++ intermediate/4/4-intermediate.py
@@ -0,0 +1,9 @@
+
+"""
+[intermediate] challenge #4
+
+Source / Reddit Post - https://www.reddit.com/r/dailyprogrammer/comments/pm6sq/2122012_challenge_4_intermediate/
+"""
+
+# The slightly cheating solution:
+print(eval(input()))
