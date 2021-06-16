a7b5eb1e9ffb43a2b01c54dc77ecc01b332f1295
KindaExists <kindaexists@gmail.com>
2021-06-17 00:07:37 +0800

    created my solution for 9-intermediate

---
+++ intermediate/9/9-intermediate.py
@@ -0,0 +1,27 @@
+
+"""
+[intermediate] challenge #9
+
+Source / Reddit Post - https://www.reddit.com/r/dailyprogrammer/comments/pu1y6/2172012_challenge_9_intermediate/
+"""
+
+
+# Not going to lie, this is a cheap method but it works
+
+if __name__ == '__main__':
+    text = ''
+
+    find_content = 'I LIEK CHOCOLATE MILK'
+    with open('intermediate/9/words.txt', 'r') as fp:
+        text = fp.read()
+        fp.close()
+
+
+    # Actually it would be better if this was done in one go
+    # However the phrasing of this challenge most likely entails(means) this shouldn't be done
+
+    replace_content = 'I quite enjoy chocolate milk. hrmmm. yes.'
+    with open('intermediate/9/words.txt', 'w') as fp:
+        text = text.replace(find_content, replace_content)
+        fp.write(text)
+        fp.close()
